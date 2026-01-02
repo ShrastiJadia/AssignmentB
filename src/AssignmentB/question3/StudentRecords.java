ad2b46bbe29a7ad3d1df22c9ba988426fb6f8d46
ShrastiJadia <jadiashrasti@gmail.com>
2026-01-02 12:53:35 +0530

    Question 3-Student Management

--- src/AssignmentB/question3/StudentRecords.java
+++ src/AssignmentB/question3/StudentRecords.java
@@ -4,62 +4,49 @@ import java.util.Scanner;
 
 public class StudentRecords {
 
-	public static void main(String[] args) 
-	{
-    StudentService st=new StudentImplement();
-    Scanner sc=new Scanner(System.in);
-    int choice;
-    try {
-    while(true)
-    {
-		System.out.println("STUDENT RECORD MANAGEMENT");
-		System.out.println("1. add student records ");
-		System.out.println("2. remove student by their roll number");
-		System.out.println("3. search student by their roll number");
-		System.out.println("4. Display records of student");
-		System.out.println("5. Terminate the program");
-		System.out.println("enter the operation you want to done");
-		choice=sc.nextInt();
-		switch (choice) {
-		case 1: {
-			System.out.println("add student records");
-			st.addStudent();
-			break;
-		}
-		case 2:
-		{
-			System.out.println(" remove student by their roll number");
-			int removeroll=sc.nextInt();
-			st.removeStudent(removeroll);
-			break;
-		}
-		case 3:
-		{
-			System.out.println("search student by their roll number");
-			int search=sc.nextInt();
-			st.searchStudent(search);
-			break;
-		}
-		case 4:
-		{
-			System.out.println("display student records");
-			st.displayStudent();
-			break;
-			
-		}
-		case 5:
-		{
-			System.exit(0);
-			break;
-		}
-		default:
-			System.out.println("invalid choice");
-		}
-    }
-    }catch (Exception e) {
-  System.out.println("error is "+e.getMessage());
-    }
-    }
-	}
+    public static void main(String[] args) {
 
+        StudentService st = new StudentImplement();
+        Scanner sc = new Scanner(System.in);
+        int choice;
 
+        while (true) {
+            System.out.println("\nSTUDENT RECORD MANAGEMENT");
+            System.out.println("1. Add student records");
+            System.out.println("2. Remove student by roll number");
+            System.out.println("3. Search student by roll number");
+            System.out.println("4. Display student records");
+            System.out.println("5. Terminate the program");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    st.addStudent();
+                    break;
+
+                case 2:
+                    System.out.print("Enter roll number to remove: ");
+                    st.removeStudent(sc.nextInt());
+                    break;
+
+                case 3:
+                    System.out.print("Enter roll number to search: ");
+                    st.searchStudent(sc.nextInt());
+                    break;
+
+                case 4:
+                    st.displayStudent();
+                    break;
+
+                case 5:
+                    System.out.println("Program terminated");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+}
--- src/AssignmentB/question3/StudentService.java
+++ src/AssignmentB/question3/StudentService.java
@@ -1,10 +1,8 @@
 package AssignmentB.question3;
 
-public interface StudentService 
-{
-   void addStudent();
-   void displayStudent();
-   void removeStudent(int rollNo);
-   void searchStudent(int rollno);
-   
+public interface StudentService {
+    void addStudent();
+    void removeStudent(int rollNo);
+    void searchStudent(int rollNo);
+    void displayStudent();
 }
